4e6d7edf06e141d3a330193c0ff48cb07ed1c18b
jarupa170649 <xinthvththicarupha116@gmail.com>
2025-06-16 18:17:43 +0000

    add thing up

---
+++ cell03/ex01/multipication.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: \n"))
+mult = 0
+while mult <= 12:
+    result = num * mult
+    print(f"{num} x {mult} = {result}")
+    mult+= 1
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,5 @@
+
+while True:
+    user_input = input("What you gotta say? : " if 'user_input' not in locals() else "I got that! Anything else? : ")
+    if user_input == "STOP":
+        break
